97b07a4d7928858ad77ed913408ee38aa4dcfde2
Jose Carlos Rocha <jcr0ch4@gmail.com>
2018-11-27 12:22:28 -0200

    Adicionado arquivo .txt

--- mudar-dominio.sh
+++ mudar-dominio.sh
@@ -27,45 +27,42 @@ criandonovaconta="Criando a nova conta no dominio wheaton.com.br     "
 confidentidade="Configurando a identidade DEFAULT     "
 renomeaconta="Renomeando a conta do domino wheatonbrasil.com.br para wheaton.com.br     "
 #usuarios=$(zmprov -l gaa |grep wheatonbrasil.com.br)
-#usuarios="daniele.ti@wheatonbrasil.com.br theo.ti@wheatonbrasil.com.br"
-#larissa.ti@wheatonbrasil.com.br maximiliano.ti@wheatonbrasil.com.br ricardov.ti@wheatonbrasil.com.br eliel.ti@wheatonbrasil.com.br john.ti@wheatonbrasil.com.br rafael.ti@wheatonbrasil.com.br"
-#usuarios="eliel.ti@wheatonbrasil.com.br"
-#usuarios="rafael.ti@wheatonbrasil.com.br larissa.ti@wheatonbrasil.com.br"
-# usuarios="maximiliano.ti@wheatonbrasil.com.br"
-#usuarios="ricardov.ti@wheatonbrasil.com.br"
-# usuarios="andre.ti@wheatonbrasil.com.br fernandof.vrs@wheatonbrasil.com.br"
-usuarios="andre.ti@wheatonbrasil.com.br"
 
 for i in $usuarios
 do
     malias=$(zmprov ga $i | grep MailAlias|head -1)
-    malias2=$(zmprov ga $i|grep MailAlias|grep "@wheaton.com.br")
-
-    if [ -n $(zmprov ga $i |grep MailAlias |grep "@wheaton.com.br"|awk -F ":" '{print $2}') ]
-    then    
+    malias2=$(zmprov ga $i|grep MailAlias|grep "@wheaton.com.br"|awk -F ": " '{print $2}')
+    # Se malias2 for vazio 
+    if [ -z $malias2]
+    then 
+	    echo "Conta sem alias wheaton.com.br"
+    # Se nao for vazio 
+    else	    
 	dominio=$(echo $malias|awk -F "@" '{print $2}')
 	dominio2=$(echo $malias2|awk -F "@" '{print $2}')
-		if [ "$dominio2"=="wheaton.com.br" ]
+	if [ "$dominio2"=="wheaton.com.br" ]
 		then
-		
+			echo "Dominio wheaton.com.br encontrado ..."	
 			echo "Apagando o Alias $(echo $malias2|awk -F ":" '{print $2}') do $dominio2"
-			# Precisa ser verificado o retorno antes de proceguir.
-			rmail=$(echo $malias2|awk -F ": " '{print $2}')
+				
 			#echo "zmprov raa $i $rmail"&& echo "$criandonovaconta [ OK ] "||echo "$criandonovaconta [ Falhou ]"
-			zmprov raa $i $rmail&& echo "$(echo $criandonovaconta) [ OK ] "||echo "$criandonovaconta [ Falhou ]"
+			zmprov raa $i $malias2 && echo "$(echo $criandonovaconta) [ OK ] "||echo "$criandonovaconta [ Falhou ]"
 
 			# Precisa ser verificado se o retorno esta OK ou nao
 			identidades=$(zmprov gid $i|grep name |awk '{print $3}'|grep -v DEFAULT)
-			echo "Identidade encontrada -> $identidades"
-			for d in $identidades
-			do
-				#echo "zmprov did $i $d "&& echo "$(echo $apagandoidentidade) [ OK ] " || echo "$(echo $apagandoidentidade) [ Falhou ]"
-				zmprov did $i $d && echo "$(echo $apagandoidentidade) [ OK ] " || echo "$(echo $apagandoidentidade) [ Falhou ]"
-
-			done
-
+			if [ -z $identiddades ]
+			then
+				echo "Identidade Secundaria não encontreda para : $i"
+			else
+				echo "Identidade encontrada -> $identidades"
+				for d in $identidades
+				do
+					#echo "zmprov did $i $d "&& echo "$(echo $apagandoidentidade) [ OK ] " || echo "$(echo $apagandoidentidade) [ Falhou ]"
+					zmprov did $i $d && echo "$(echo $apagandoidentidade) [ OK ] " || echo "$(echo $apagandoidentidade) [ Falhou ]"
+				done
+	                fi
 			# Precisa verificar o retorno para as variaveis
-			aliasnovo=$(echo $malias|awk -F ": " '{print $2}')
+			#aliasnovo=$(echo $malias|awk -F ": " '{print $2}')
 			novo=$(echo $i|sed -e 's/wheatonbrasil/wheaton/g')
 	
 			#echo "zmprov ra $i $novo "&& echo "$(echo $renomeaconta) [OK]" || echo "$(echo $renomeaconta) [ Falhou ]"
@@ -77,7 +74,8 @@ do
 			zmprov mid $novo DEFAULT zimbraPrefFromAddress $novo1 zimbraPrefReplyToAddress $novo1 zimbraPrefWhenSentToAddresses $novo1 zimbraPrefWhenSentToEnabled TRUE && echo "$(echo $confidentidade) [ OK ]" || echo "$(echo $confidentidade) [ Falhou ]"
 
 			#echo "zmprov aaa $novo $aliasnovo "&& echo "$aliasdominio [ OK ]" || echo "$aliasdominio [ Falhou ]"
-			zmprov aaa $novo $aliasnovo && echo "$(echo $aliasdominio) [ OK ]" || echo "$(echo $aliasdominio) [ Falhou ]"
+			zmprov aaa $novo $malias2 && echo "$(echo $aliasdominio) [ OK ]" || echo "$(echo $aliasdominio) [ Falhou ]"
+
 	
 		fi
     fi
